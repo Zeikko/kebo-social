77edf3812127a5e76663e0373a770e8fc0aaea20
Peter <Peter@Peter-PC>
2013-10-19 20:10:57 +0100

    added partial view files

---
+++ views/twitter/_feed-footer.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * View File - Twitter Feed Footer
+ */
+?>
+
+    </ul><!-- .ktweets -->
+
+<?php do_action( 'kbso_after_twitter_feed', $tweets, $instance, $widget_id ); ?>
---
+++ views/twitter/_feed-header.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * View File - Twitter Feed Header
+ */
+?>
+
+<?php do_action( 'kbso_before_twitter_feed', $tweets, $instance, $widget_id ); ?>
+
+    <ul class="kebo-tweets dark ktweets">
